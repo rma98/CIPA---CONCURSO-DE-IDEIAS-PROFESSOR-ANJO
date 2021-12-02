4c34050e8fc2c633bd91426ae7f23a0e95bb706e
PMR-216803\PROEJA <ctic@palmares.ifpe.edu.br>
2021-12-02 11:32:41 -0300

    Atualiza US3-CATEGORIA

--- cipa-web/src/App.vue
+++ cipa-web/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <categoria/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import categoria from './components/categoria.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    categoria
   }
 }
 </script>
