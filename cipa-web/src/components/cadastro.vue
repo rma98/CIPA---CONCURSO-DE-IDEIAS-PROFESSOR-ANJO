a129310bea945c59d7f79a4cb755322dfd483e27
Andrielly <afl5@discente.ifpe.edu.br>
2021-12-11 20:03:32 -0300

    Atualiza tela de cadastro

--- cipa-web/src/components/cadastro.vue
+++ cipa-web/src/components/cadastro.vue
@@ -1,166 +1,225 @@
 <template>
-<div class="Principal">
-  <div class="content1">
-<form class="forma">
-<img class="img form-baixar" src="https://i.imgur.com/HcLNjzn.png">
-<div class="global">
-<h4 class="color">
-     Olá!
-     </h4>
-     
-     <p class="p">
-     Por favor insira seus<br>
-     dados para se cadastrar</p>
-     </div>
-     <div id="global">
-     <p id="p">
-     Nome completo
-     </p>
-     <input id="inp" type="text" >
-      <p id="p">
-     E-mail institucional
-     </p>
-     <input id="inp" type="email" >
-      <p id="p">
-     Senha
-     </p>
-     <input id="inp" type="password" >
-     <p class="smh">
-     Você esqueceu a senha?
-     </p>
-     <br><br>
-
-    <a class="waves-effect waves-light btn" style="background:#1c2c44">CADASTRAR</a>
-    
-    
-    
+<div class="content-index">
+<div class="division-1">
+    <img src="https://i.imgur.com/HcLNjzn.png" class="logo">
+    <div class="texts">
+        <p><mark>Olá</mark><br>por favor insira os dados<br>
+        do projeto para se cadastrar.</p>
     </div>
-</form>
+    <div class="agroup">
+      <div class="row">
+             <div class="input-field col s6">
+          <i class="material-icons prefix">person</i>
+          <input id="icon_prefix" type="text" class="validate" placeholder=" Nome completo:">
+        </div>
+        </div>
+      <div class="row">
+             <div class="input-field col s6">
+          <i class="material-icons prefix">email</i>
+          <input id="icon_prefix" type="text" class="validate" placeholder="E-mail institucional:">
+        </div>
+        </div>
+          <div class="row">
+          <div class="input-field col s6">
+          <i class="material-icons prefix">vpn_key</i>
+          <input id="icon_prefix2" type="text" class="validate" placeholder="Senha:">
+        </div>
+        </div>
+         <div class="row2">
+         
+         <button class="btn waves-effect waves-light" type="submit" name="action">CADASTRAR
+        <i class="material-icons right">send</i>
+        </button>   
+    </div>  
+    <div class="media-icons">
+      <i class="fa-brands fa-facebook"></i>
+      <i class="fa-brands fa-twitter"></i>
+      <i class="fa-brands fa-google-plus-g"></i>
     </div>
-<div class="content2" style="background:#CDDAE2;">
-  <img id="pictures" src="https://i.imgur.com/EUoiruM.png">
-  </div>
-  </div>
-
-
+                        </div>
+                    </div>
+                <div class="division-2">
+                    <img src="undraw_product_teardown_elol-removebg-preview.png">
+                  <h1 class="titulo">CIPA</h1>
+                  <p class="subtitulo">Concurso de ideias<br>
+ professor anjo</p>
+            </div>
+        </div>
 </template>
 
 <script>
 export default {
-  name: 'cadastro',
+  name: 'Login',
   props: {
     msg: String
   }
 }
 </script>
 
-
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;1,400&display=swap');
-
-html,
-body,
-table,
-input,
-button {
-  font-family: 'Poppins', sans-serif;
-}
-#entre{
-
-}
-body{
-  width: 100vw;
-}
-.tela {
- margin-left: -20%;
+        @import url('https://fonts.googleapis.com/css2?family=Poppins&family=Rubik&family=Rubik+Mono+One&display=swap');;
+            * {
+    margin: 0;
+    padding: 0;
+  }
+  html{
+    padding: 0;
+    margin: 0;
+  }
+  #app{
+    margin: 0;
+    padding: 0;
+  }
   
+  .content-index {
+    display: flex;
+    width: 100%;
+    height: 100vh;
+  }
+  .media-icons{
+    margin-left: 15%;
+    margin-top: 15%;
+  }
+  .subtitulo{
+    font-family: 'Rubik', sans-serif;
+    font-size: 47px;
+    text-shadow: 4px 4px 4px #000000;
+    margin-left: 30%;
+    }
+  .media-icons i{
+    font-size: 2.8rem;
+    margin-left: 14%;
+  }
+  .fa-google-plus-g{
+    color:#e34133;
+  }
+  .fa-twitter{
+    color: #00acee;
+  }  
+  .fa-facebook{
+    color: #3b5998;
+  }
+  .division-1 {
+    width:40vw;
+  }
+  .division-1 img{
+    width: 280px;
+    margin-left: 34%;
+  }
+  .texts p{
+      font-family: 'Poppins', Arial;
+      font-size: 24px;
+      color: #1B2F49;
+      text-align: justify;
+  }
+  .texts mark{
+      background-color: #fff;
+      color: #1B2F49;
+      font-weight: bold;
+      font-size: 26px;
+  }
+  .logo{
+    margin-bottom: 6%;
+  }
+  .texts{
+      margin-left: 27%;
+      margin-top: -8%;
+  }
+  .agroup{
+      margin-top: 5% !important;
+  }
+  
+  .row {
+    margin-left:20% !important;
+    width: 100vh;
+  }
+  .row2{
+    margin-left: 38%!important;
+    margin-top: 5%;
+  }
+  .row2 button{
+      background-color: #1B2F49 !important;
+      padding-left: 38px;
+      padding-right: 35px;
+  }
+  
+  .division-2 {
+    background-color: #CDDAE2;
+    margin-top: -4%;
+    width: 100vw;
+    height:113vh;
+  }
+  .division-2 img{
+      width: 680px;
+      margin-top: 6%;
+      margin-left: 15%;
+  }
+  .titulo{
+    font-family: 'Rubik Mono One', sans-serif;
+    font-size: 86px;
+    margin-top: -4%;
+    margin-left: 32%!important;
+  }
+input{
+  border: solid 0.3vh #1c2c44 !important;
+  text-align: left !important;
+  height: 30px !important;
+  margin-top: 2% !important;
+  border-radius: 5px !important;
 }
-.img{
-      width: 280px;
-      margin-left: 24%;
-}
-.color {
-  color: #1c2c44;
-  font-weight: bold;
-
-}
-
-.p {
-  color: #1c2c44;
-}
-
-#p {
-  margin-top: 3px;
-}
-
-#inp {
-  border: solid 2px #1c2c44;
-  height: 35px;
-  width: 305px;
-  margin-top: px;
-  border-radius: 5px;
-}
-
-.btn {
-  background: #1c2c44;
-  margin-left: 1%;
-  margin-top: -35%;
-  border-radius: 5px;
-  width: 305px;
-
-}
-
-.global {
-  margin-left: 15%;
-  margin-right: 28%;
-}
-
-.forma {
-  background: white;
-  width: 450px;
-  margin-left: 14%;
-}
-
-.Principal{
-  display: flex;
-}
-.content1{
-  width: 70vw;
-  height: 70vh;
-}
-.content2{
-  width: 70vw;
-  height: 100vh;
-}
-.smh {
-  color: #1c2c44;
-}
-
-.smh:hover {
-  color: blue;
-}
-
-#global {
-  margin-left: 15%;
-  margin-right: 28%;
-}
-
-#vlr {
-  Width: 200px;
-  Margin-top: 15px;
-  margin-left:2%;
- 
-}
-h4{
-  font-size: 2.28rem;
-    line-height: 110%;
-    margin: -1.48rem 0 .912rem 0;
-}
-
-#pictures{
-  width: 680px;
-   margin-top: 4%;
-  margin-left: 4%;
-}
-
+  @media (max-width: 580px){
+    .division-2{
+      background-color: #fff;
+    }
+    .content-index{
+      border: 2px solid black;
+      height: 105vh;
+      width: 520px;
+      margin-left: 4%!important;
+    }
+    .logo{
+    margin-bottom: 6%;
+  }
+  .division-1 img{
+    width: 240px;
+    margin-left: 25%;
+  }
+  .media-icons{
+    margin-left: 10%;
+    margin-top: 6%;
+  }
+  .media-icons i{
+    font-size: 2.8rem;
+    margin-left:14%;
+  }
+  .texts{
+      margin-left: 16%;
+      margin-top:-6%;
+  }
+  .agroup{
+      margin-top: 5% !important;
+  }
+  
+  .row {
+    margin-left:11% !important;
+    width: 100vh;
+  }
+  .row2{
+    margin-left:31%!important;
+  }
+  .row2 button{
+      background-color: #1B2F49 !important;
+      padding-left: 35px;
+      padding-right: 35px;
+  }
+  .division-2 img{
+      width: 0px;
+      margin-top: 17%;
+      margin-left: 18%;
+  }
+  .content-index{
+    margin-left:13%;
+  }
+  }
 </style>
