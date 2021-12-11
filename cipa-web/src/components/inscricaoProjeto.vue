3cb24cb86b30329d90ea5ff17e35fe185f617fba
Robson <rma@a.palmares.ifpe.edu.br>
2021-12-11 20:06:55 -0300

    Novas alterações na tela de inscrição do projeto

--- cipa-web/src/components/inscricaoProjeto.vue
+++ cipa-web/src/components/inscricaoProjeto.vue
@@ -1,224 +1,243 @@
 <template>
-  <div class="row cor-fundo">
-    <div class="col s6">
-      <div>
-        <form class="form-inscricao-projeto flow-text">
-          <img src="https://i.imgur.com/HcLNjzn.png">
-          <div class="titulo">
-            <h1>
-              <strong>
-                Bem vindo ao CIPA.
-              </strong>
-            </h1>
-          </div>
-          <div class="paragrafo">
-            <p>
-              Por favor insira ps dados
-            </p>
-          </div>
-          <div id="cont-paragrafo">
-            do projeto para se inscrever
-          </div>
-          <div class="subtitulos">
-            <label>
-              Nome do Líder:
-            </label>
-          </div>
-          <div class="sub-inputs">
-            <input type="text" name="nomeLider" required>
-          </div>
-          <div class="subtitulos">
-            <label>
-              Nome dos integrantes (Separar por vírgula)
-            </label>
-          </div>
-          <div class="sub-inputs">
-            <input type="text" name="nomeIntegrantes">
-          </div>
-          <div class="select-categoria">
-          <label id="cat-titulo">
-            categoria
-          </label>
-        </div>
-        <div class="select-use">
-          <select class="browser-default">
-            <option value="" disabled selected>SELECIONE UMA CATEGORIA</option>
-            <option value="1">ERRADIAÇÃO DA POBREZA</option>
-            <option value="2">FOME ZERO</option>
-            <option value="3">BOA SAÚDE E BEM-ESTAR</option>
-            <option value="4">EDUCAÇÃO DE QUALIDADE</option>
-            <option value="5">IGUALDADE DE GÊNERO</option>
-            <option value="6">ÁGUA LIMPA E SANIAMENTO</option>
-            <option value="7">ENERGIA ACESSÍVEL E LIMPA</option>
-            <option value="8">EMPREGO DIGNO E CRESCIMNENTO ECONÔMICO</option>
-            <option value="9">INDÚSTRIA, INOVAÇÃO E INFRAESTRUTURA</option>
-            <option value="10">REDUÇÃO DAS DESIGUALDADES</option>
-            <option value="11">CIDADES E COMUNIDADES SUSTENTÁVEIS</option>
-            <option value="12">CONSUMO E PRODUÇÃO RESPONSÁVEIS</option>
-          </select>
-        </div>
-        <div class="subtitulos">
-          <label>
-            Título do projeto:
-          </label>
-        </div>
-        <div class="sub-inputs">
-          <input type="text" name="titulo" required>
-        </div>
-        <div class="subtitulos">
-          <label>
-            Link do vídeo: (Opcional)
-          </label>
-        </div>
-        <div class="sub-inputs">
-          <input type="text" name="linkVideo">
-        </div>
-        <div class="subtitulos">
-          <label>
-            DESCREVA SEU PROJETO:
-          </label>
-        </div>
-        <div class="descricao">
-          <textarea maxlength="800" name="descrendoProjeto" required></textarea>
-        </div>
-        <div class="botao">
-          <button class="btn waves-effect waves-light" type="submit" name="action">INSCREVER </button>
-        </div>
-        </form>
-      </div>
+<div class="content-index">
+<div class="division-1">
+    <img src="https://i.imgur.com/HcLNjzn.png" class="logo">
+    <div class="texts">
+        <p><mark>Bem vindo ao CIPA.</mark><br>por favor insira os dados<br>
+        do projeto para se inscrever.</p>
     </div>
-    <div class="col s6">
-      <img id="foto" src="undraw_product_teardown_elol-removebg-preview.png">
-      <div class="title">
-        <h1> 
-          CIPA
-        </h1>
-      </div>
-      <div class="sub-title">  
-        <P>
-          Concurso de ideias
-        </P>
-      </div>  
+    <div class="agroup">
+      <div class="row">
+             <div class="input-field col s6">
+          <i class="material-icons prefix">person</i>
+          <input id="icon_prefix" type="text" class="validate" placeholder="Nome do Líder:" required>
+        </div>
+        </div>
+      <div class="row">
+             <div class="input-field col s6">
+          <i class="material-icons prefix">group</i>
+          <input id="icon_prefix" type="text" class="validate" placeholder="Nome dos integrantes:">
+        </div>
+        </div>
+        <div class="row">
+             <div class="input-field col s6">
+          <i class="material-icons prefix">category</i>
+          <input id="icon_prefix" type="text" class="validate" placeholder="Categoria:">
+        </div>
+        </div>
+      <div class="row">
+             <div class="input-field col s6">
+          <i class="material-icons prefix">title</i>
+          <input id="icon_prefix" type="text" class="validate" placeholder="Título do projeto:" required >
+        </div>
+        </div>
+          <div class="row">
+          <div class="input-field col s6">
+          <i class="material-icons prefix">link</i>
+          <input id="icon_prefix2" type="text" class="validate" placeholder="Link do vídeo:">
+        </div>
+        </div>
+          <div class="row">
+          <div class="input-field col s6">
+          <i class="material-icons prefix">description</i>
+          <textarea maxlength="800" name="descrendoProjeto" id="descrendoProjeto" cols="30" rows="10" required ></textarea> 
+        </div>
+        </div>
+         <div class="row2">
+         
+         <button class="btn waves-effect waves-light" type="submit" name="action">INSCREVER
+        <i class="material-icons right">send</i>
+        </button>   
+    </div>  
+    <div class="media-icons">
+      <i class="fa-brands fa-facebook"></i>
+      <i class="fa-brands fa-twitter"></i>
+      <i class="fa-brands fa-google-plus-g"></i>
     </div>
-  </div>
+                        </div>
+                    </div>
+                <div class="division-2">
+                    <img src="undraw_product_teardown_elol-removebg-preview.png">
+                  <h1 class="titulo">CIPA</h1>
+                  <p class="subtitulo">Concurso de ideias<br>
+ professor anjo</p>
+            </div>
+        </div>
 </template>
 
 <script>
 export default {
-  name: 'inscricaoProjeto',
+  name: 'Login',
   props: {
     msg: String
   }
 }
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;1,400&display=swap');
-
-html, body, input, select, button {
-  font-family: 'Poppins', sans-serif;
-  margin: 0;
-  padding: 0;
+        @import url('https://fonts.googleapis.com/css2?family=Poppins&family=Rubik&family=Rubik+Mono+One&display=swap');;
+            * {
+    margin: 0;
+    padding: 0;
+  }
+  html{
+    padding: 0;
+    margin: 0;
+  }
+  #app{
+    margin: 0;
+    padding: 0;
+  }
+  
+  .content-index {
+    display: flex;
+    width: 100%;
+    height: 100vh;
+  }
+  .media-icons{
+    margin-left: 15%;
+    margin-top: 15%;
+  }
+  .subtitulo{
+    font-family: 'Rubik', sans-serif;
+    font-size: 47px;
+    text-shadow: 4px 4px 4px #000000;
+    margin-left: 30%;
+    }
+  .media-icons i{
+    font-size: 2.8rem;
+    margin-left: 14%;
+  }
+  .fa-google-plus-g{
+    color:#e34133;
+  }
+  .fa-twitter{
+    color: #00acee;
+  }  
+  .fa-facebook{
+    color: #3b5998;
+  }
+  .division-1 {
+    width:40vw;
+  }
+  .division-1 img{
+    width: 280px;
+    margin-left: 34%;
+  }
+  .texts p{
+      font-family: 'Poppins', Arial;
+      font-size: 24px;
+      color: #1B2F49;
+      text-align: justify;
+  }
+  .texts mark{
+      background-color: #fff;
+      color: #1B2F49;
+      font-weight: bold;
+      font-size: 26px;
+  }
+  .logo{
+    margin-bottom: 6%;
+  }
+  .texts{
+      margin-left: 27%;
+      margin-top: -8%;
+  }
+  .agroup{
+      margin-top: 5% !important;
+  }
+  
+  .row {
+    margin-left:20% !important;
+    width: 100vh;
+  }
+  .row2{
+    margin-left: 38%!important;
+    margin-top: 5%;
+  }
+  .row2 button{
+      background-color: #1B2F49 !important;
+      padding-left: 38px;
+      padding-right: 35px;
+  }
+  
+  .division-2 {
+    background-color: #CDDAE2;
+    margin-top: -4%;
+    width: 100vw;
+    height:113vh;
+  }
+  .division-2 img{
+      width: 680px;
+      margin-top: 6%;
+      margin-left: 15%;
+  }
+  .titulo{
+    font-family: 'Rubik Mono One', sans-serif;
+    font-size: 86px;
+    margin-top: -4%;
+    margin-left: 32%!important;
+  }
+input{
+  border: solid 0.3vh #1c2c44 !important;
+  text-align: left !important;
+  height: 30px !important;
+  margin-top: 2% !important;
+  border-radius: 5px !important;
 }
-.form-inscricao-projeto, .flow-text {
-  border: solid 1px #1B2F49;
-  width: 49vw;
-  height: 154vh;
-  background-color: #ffffff;
-}
-img {
-  width: 20vw;
-  margin-left: 25%;
-}
-.titulo {
-  color: #1B2F49;
-}
-.titulo, h1 {
-  font-size: 30pt;
-  margin-top: -5%;
-  margin-left: 11%;
-}
-.paragrafo {
-  color: #1B2F49;
-}
-.paragrafo p {
-  font-size: 30pt;
-  margin-top: -3%;
-  margin-left: 18%;
-}
-#cont-paragrafo {
-  color: #1B2F49;
-  font-size: 30pt;
-  margin-top: -5%;
-  margin-left: 18%;
-}
-.subtitulos label {
-  color: #1B2F49;
-  font-size: 20pt;
-  margin-left: 18%;
-}
-.sub-inputs input {
-  color: #1B2F49;
-  width: 32vw;
-  height: 6vh;
-  margin-top: 6px;
-  margin-left: 18%;
-  margin-bottom: 3%;
-  border: solid 1px #1B2F49;  
-}
-#cat-titulo {
-  color: #1B2F49;
-  font-size: 20pt;
-  margin-left: 20%;
-}
-.select-use select{
-  color: #1B2F49;
-  width: 20vw;
-  height: 6vh;
-  margin-top: 6px;
-  margin-left: 18%;
-  margin-bottom: 3%;
-  border: solid 1px #1B2F49; 
-}
-.descricao {
-  color: #1B2F49;
-  width: 32vw;
-  margin-top: 6px;
-  height: 7vh;
-  margin-left: 18%;
-}
-.btn {
-  background: #1B2F49;
-  color: #ffffff;
-  width: 32vw;
-  height: 8vh;
-  margin-top: 6px;
-  margin-left: 18%;
-}
-#foto {
-  width: 43vw;
-  margin-top: 4%;
-  margin-left: 12%;
-}
-.cor-fundo {
-  background-color: #cddae2;
-}
-.title {
-  margin-left: 30%;
-  font-weight: bold;
-  font-family: 'Rubik Mono One', sans-serif;
-  color: rgba(0,0,0,0.87);
-}
-.title h1 {
-  font-size: 86px;
-}
-.sub-title {
-  font-family: 'Rubik', sans-serif;
-  text-shadow: 4px 4px 4px #000000;
-  margin-left: 32%;
-}
-.sub-title p {
-  font-size: 46px;
-  margin-top: 0%;
-}
+  @media (max-width: 580px){
+    .division-2{
+      background-color: #fff;
+    }
+    .content-index{
+      border: 2px solid black;
+      height: 105vh;
+      width: 520px;
+      margin-left: 4%!important;
+    }
+    .logo{
+    margin-bottom: 6%;
+  }
+  .division-1 img{
+    width: 240px;
+    margin-left: 25%;
+  }
+  .media-icons{
+    margin-left: 10%;
+    margin-top: 6%;
+  }
+  .media-icons i{
+    font-size: 2.8rem;
+    margin-left:14%;
+  }
+  .texts{
+      margin-left: 16%;
+      margin-top:-6%;
+  }
+  .agroup{
+      margin-top: 5% !important;
+  }
+  
+  .row {
+    margin-left:11% !important;
+    width: 100vh;
+  }
+  .row2{
+    margin-left:31%!important;
+  }
+  .row2 button{
+      background-color: #1B2F49 !important;
+      padding-left: 35px;
+      padding-right: 35px;
+  }
+  .division-2 img{
+      width: 0px;
+      margin-top: 17%;
+      margin-left: 18%;
+  }
+  .content-index{
+    margin-left:13%;
+  }
+  }
 </style>
